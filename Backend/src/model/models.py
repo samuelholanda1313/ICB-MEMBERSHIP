1e6b1382c55c28f672647e75224457532e33a83f
artursousac <artursousac@hotmail.com>
2024-11-09 20:14:12 -0300

    criacao do model para a entidade de membros

--- Backend/src/model/models.py
+++ Backend/src/model/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, CheckConstraint
 from sqlalchemy.orm import relationship
 from src.database.database import Base
 
@@ -10,5 +10,26 @@ class Administrador(Base):
     tipo = Column(String(255), nullable=False)
     unidade_id = Column(Integer, ForeignKey("unidades.id"), nullable=False)
     membros_id = Column(Integer, ForeignKey("membros.id"), nullable=False)
-    unidade = relationship("Unidade", back_populates="administradores")
-    membro = relationship("Membro", back_populates="administradores")
+    unidade = relationship("Unidade", back_populates="Administrador")
+    membro = relationship("Membro", back_populates="Administrador")
+
+# Criação do Model para a entidade de Membro
+class Membro(Base):
+    __tablename__ = "membros"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    data_nascimento = Column(Date, nullable=False)
+    sexo = Column(String(10), CheckConstraint("sexo IN ('Masculino', 'Feminino')"), nullable=False)
+    unidade_id = Column(Integer, ForeignKey("unidades.id"))
+    posicao = Column(String(255), nullable=False)
+    endereco = Column(String(255), nullable=False)
+    bairro = Column(String(255), nullable=False)
+    estado_civil = Column(String(255), nullable=False)
+    telefone = Column(String(255), nullable=False)
+    profissao = Column(String(255), nullable=False)
+    data_conversao = Column(Date)
+    batismo = Column(String(255), nullable=False)
+    data_batismo = Column(Date)
+    unidade = relationship("Unidade", back_populates="Membro")
+    administradores = relationship("Administrador", back_populates="Membro")
