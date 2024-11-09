ad2467e72bfd983fe2401a842718054d99873123
artursousac <artursousac@hotmail.com>
2024-11-09 18:57:56 -0300

    continuacao do backend, ajuste no gitignore, e definicao do arquivo database

--- Backend/src/database/database.py
+++ Backend/src/database/database.py
@@ -4,10 +4,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
+# URL do Banco de Dados
 URL_DATABASE = f'postgresql://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASS")}@localhost:5432/ICB'
 
 engine = create_engine(URL_DATABASE)
-
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
