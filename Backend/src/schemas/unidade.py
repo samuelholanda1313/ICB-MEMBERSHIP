902d2932b038cb5fb79781211b1f408f33967ad4
artursousac <artursousac@hotmail.com>
2024-11-09 21:04:50 -0300

    desenvolvimento do schema da entidade de unidade

--- Backend/src/schemas/unidade.py
+++ Backend/src/schemas/unidade.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 from typing import List
+from src.schemas.membro import Membro
+from src.schemas.administrador import Administrador
 
 # Criação da base para a entidade de Unidade. A criação utilizará este mesmo schema.
 class UnidadeBase(BaseModel):
@@ -12,6 +14,8 @@ class UnidadeBase(BaseModel):
 
 class Unidade(UnidadeBase):
     id: int
+    membros: List[Membro] = []
+    administradores: List[Administrador] = []
 
     class Config:
         orm_mode = True
