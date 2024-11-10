316345fc70bc4871553bd0f7ec77562279903b11
artursousac <artursousac@hotmail.com>
2024-11-09 21:00:50 -0300

    desenvolvimento do schema da entidade de administrador

--- Backend/src/schemas/administrador.py
+++ Backend/src/schemas/administrador.py
@@ -1,21 +1,22 @@
 from pydantic import BaseModel
-from typing import Optional
 from src.schemas.unidade import Unidade
+from src.schemas.membro import Membro
 
 # Define um schema base para a entidade Administrador
 class AdministradorBase(BaseModel):
-    membro_id: int
     tipo: str
-    unidade_id: int
 
 # Define o schema para caso de criar novo administrador, herdando atributos do AdministradorBase
 class AdministradorCreate(AdministradorBase):
     senha: str
+    membro_id: int
+    unidade_id: int
 
 # Define o schema para caso de ler informações do administrador
 class Administrador(AdministradorBase):
     id: int
     unidade: Unidade
+    membro: Membro
 
     class Config:
         orm_mode = True
