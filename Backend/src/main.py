263e06a3bcb3a7ecbcc66f03478f4b67dc071d78
artursousac <artursousac@hotmail.com>
2024-11-10 23:23:17 -0300

    .

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from src.database.database import Base
+from src.database.database import engine
+from src.routes.administradores_routes import router as administradores_router
+#from src.routes.membros_routes import router as membros_router
+#from src.routes.unidades_routes import router as unidades_router
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,4 +18,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#app.include_router(router)
+app.include_router(administradores_router, prefix="/administradores", tags=["Administradores"])
+#app.include_router(membros_router, prefix="/membros", tags=["Membros"])
+#app.include_router(unidades_router, prefix="/unidades", tags=["Unidades"])
