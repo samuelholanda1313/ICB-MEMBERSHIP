4144bbf1b3c7c04451a0a35e66cde41416f52d7e
artursousac <artursousac@hotmail.com>
2024-11-16 16:28:29 -0300

    .

--- src/routes/membros_routes.py
+++
@@ -1,7 +0,0 @@
-from src.schemas.schemas import Membro, MembroCreate
-from src.database.database import get_db
-from fastapi import APIRouter, HTTPException, Depends, Query
-from sqlalchemy.orm import Session, joinedload
-from src.model import models
-
-router = APIRouter()
